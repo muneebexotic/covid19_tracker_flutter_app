cf542f4263fb75e7ce11db617f7166b00c79a855
Muneeb-Usman~ <muneebusman54@gmail.com~>
2024-09-08 10:11:43 +0500

    created detail_screen for country wise stats

--- lib/Services/states_services.dart
+++ lib/Services/states_services.dart
@@ -18,4 +18,16 @@ class StatesServices {
 
     }
   }
+  Future<List<dynamic>> countriesListApi () async {
+    var data ;
+    final response = await http.get(Uri.parse(AppUrl.countriesList));
+
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception('Error');
+
+    }
+  }
 }
